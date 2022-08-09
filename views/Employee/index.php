4b1dc0f99ddf8e954d226c8cf2de10c2347bde9b
Khushbu <Khushbusingh786khsingh.com>
2022-08-09 16:02:54 +0530

    views

---
+++ views/Employee/index.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Employees';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="employee-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Employee', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'username',
+           // 'auth_key',
+           // 'password',
+            //'date_of_birth',
+            //'phone',
+           'email:email',
+            'state',
+            'district',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action,  $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
